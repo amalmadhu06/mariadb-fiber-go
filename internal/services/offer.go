788c14eb743c703f7fc06eaa0ebac95dd696d213
Amal <amalmadhu06@gmail.com>
2023-07-20 22:50:27 +0530

    feat(api): Add endpoint for fetching offers bases on country

---
+++ internal/services/offer.go
@@ -0,0 +1,37 @@
+package services
+
+import (
+	"context"
+	"github.com/amalmadhu06/mariadb-fiber-go/internal/entities"
+	mem "github.com/amalmadhu06/mariadb-fiber-go/internal/memory/interfaces"
+	repo "github.com/amalmadhu06/mariadb-fiber-go/internal/repository/interfaces"
+	svc "github.com/amalmadhu06/mariadb-fiber-go/internal/services/interfaces"
+)
+
+type OfferUsecase struct {
+	offerRepo   repo.OfferRepo
+	offerMemory mem.OfferMemory
+}
+
+func NewOfferUsecase(offerRepo repo.OfferRepo, offerMem mem.OfferMemory) svc.OfferUsecase {
+	return &OfferUsecase{
+		offerRepo:   offerRepo,
+		offerMemory: offerMem,
+	}
+}
+
+func (o *OfferUsecase) GetOffer(ctx context.Context, country string) ([]entities.OfferCompany, error) {
+	// if data is present in memory, retrieve and send it
+	offer, err := o.offerMemory.GetOffer(ctx, country)
+	if err != nil {
+		return nil, err
+	}
+
+	if offer != nil {
+		return offer, nil
+	}
+
+	// if data is not present in memory, retrieve it from the database
+	return o.offerRepo.GetOffer(ctx, country)
+
+}
---
+++ internal/web/handler/offer.go
@@ -0,0 +1,47 @@
+package handler
+
+import (
+	"context"
+	"github.com/amalmadhu06/mariadb-fiber-go/internal/services/interfaces"
+	"github.com/gofiber/fiber/v2"
+	"go.mongodb.org/mongo-driver/bson"
+	_ "go.mongodb.org/mongo-driver/bson"
+	"net/http"
+)
+
+type OfferHandler struct {
+	offerUsecase interfaces.OfferUsecase
+}
+
+func NewOfferHandler(usecase interfaces.OfferUsecase) *OfferHandler {
+	return &OfferHandler{
+		offerUsecase: usecase,
+	}
+}
+
+func (o *OfferHandler) GetOffer(c *fiber.Ctx) error {
+	country := c.Params("country")
+	// Todo : Pagination
+	if country == "" {
+		return c.Status(http.StatusBadRequest).JSON(fiber.Error{
+			Message: "country name must be included in query",
+		})
+	}
+
+	respFormat := c.Get("Accept")
+
+	offers, err := o.offerUsecase.GetOffer(context.TODO(), country)
+	if err != nil {
+		return fiber.NewError(http.StatusInternalServerError, "failed to retrieve data")
+	}
+
+	if respFormat == "application/bson" {
+		_, offerBytes, err := bson.MarshalValue(offers)
+		if err != nil {
+			return fiber.NewError(http.StatusInternalServerError, "failed to marshal bson data")
+		}
+		return c.Status(http.StatusOK).Send(offerBytes)
+	}
+	// keeping json as the default response format
+	return c.Status(http.StatusOK).JSON(offers)
+}
