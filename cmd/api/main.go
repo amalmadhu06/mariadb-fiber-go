b6fae2c2e4eb811bfbb5697e3207628e0ae471ea
Amal <amalmadhu06@gmail.com>
2023-07-20 22:49:17 +0530

    feat(api): Implement graceful shutdown

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,22 +4,41 @@ import (
 	"github.com/amalmadhu06/mariadb-fiber-go/internal/di"
 	"github.com/amalmadhu06/mariadb-fiber-go/pkg/config"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
 
-	// load configuration values
+	// loading required configuration values
 	cfg, err := config.LoadConfig()
 	if err != nil {
 		log.Fatal("failed to load config: ", err)
 	}
 
-	// initialize apis using dependencies
-	server, err := di.InitializeAPI(cfg)
+	// injecting dependencies and initializing server
+	server, err := di.InjectDependencies(cfg)
 	if err != nil {
 		log.Fatal("failed to start server: ", err)
 	}
 
-	// start the server
-	server.Start()
+	// channel for sending server shutdown signal
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, os.Interrupt, syscall.SIGTERM)
+
+	// starting server in a separate go routine
+	go func() {
+		server.Start()
+	}()
+
+	// receiving server shutdown signal
+	sig := <-signalCh
+	log.Println("Received signal : ", sig)
+
+	// graceful shutdown of the server
+	if err := server.ShutDown(); err != nil {
+		log.Fatalf("Server shutdown failed: %v\n", err)
+	}
+
 }
